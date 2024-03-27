875af2faa1909a9cd15eb4c9a92a2a9a564002dd
andreimacavei <andrei.macavei89@gmail.com>
2024-03-27 13:34:07 +0200

    Make Usage page show user's available credits

--- src/app/lib/data.js
+++ src/app/lib/data.js
@@ -25,3 +25,44 @@ export const fetchAllData = async () => {
 
   return userDomains;
 };
+
+export const fetchUserData = async () => {
+  const session = await getServerSession(authOptions);
+  if (!session || !session.user) return null;
+
+  const { user } = session;
+  const userData = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+  });
+  console.log("userData: ", userData);
+
+  // Create a join query to fetch user subscription plan
+  const userSubscriptionPlan = await prisma.user.findUnique({
+    where: {
+      id: user.id,
+    },
+    select: {
+      subscription: {
+        select: {
+          plan: true,
+        },
+      },
+    },
+  });
+  console.log("userSubscriptionPlan: ", userSubscriptionPlan);
+
+  // const userDomains = await prisma.domains.findMany({
+  //   where: {
+  //     user: {
+  //       id: user.id,
+  //     },
+  //   },
+  // });
+
+  return {
+    userCredits: userData.credits,
+    subscriptionPlan: userSubscriptionPlan.subscription.plan,
+  };
+};
--- src/app/ui/UsageCard.tsx
+++ src/app/ui/UsageCard.tsx
@@ -1,35 +1,67 @@
 'use client';
-import { ProgressBar } from '@/app/ui';
+// import { ProgressBar } from '@/app/ui';
 import { useEffect, useState } from 'react';
-import { set } from 'zod';
+import { fetchUserData } from '@/app/lib/data';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function UsageCard() {
   const [credits, setCredits] = useState(0)
+  const [planCredits, setPlanCredits] = useState(0)
   const [loading, setLoading] = useState(true)
+  const [plan, setPlan] = useState({
+    name: '',
+    creditsPerMonth: 0,
+    domainsAllowed: 0
+  })
 
   useEffect(() => {
     (async () => {
       setLoading(true)
-      // Fetch credits from the server
+      // Fetch user data from the server
+      let data = await fetchUserData();
+      setLoading(false);
 
-      setLoading(false)
-
-      setCredits(1500)
+      setCredits(data.userCredits);
+      setPlanCredits(data.subscriptionPlan.creditsPerMonth);
+      setPlan({
+        name: data.subscriptionPlan.name,
+        creditsPerMonth: data.subscriptionPlan.creditsPerMonth,
+        domainsAllowed: data.subscriptionPlan.domainsAllowed
+      })
+      
     })()
   }, [])
 
+  const handleClick = () => {
+    console.log('Upgrade plan')
+  }
+
   return (
-    <div className="col-span-1 auto-rows-min grid-cols-1 lg:col-span-5 mt-4">
+    <div className="col-span-1 auto-rows-min grid-cols-1 lg:col-span-5">
       {/* <div className="bg-white shadow-md rounded-md p-6"> */}
-      <div className="flex gap-4 mt-4 items-center border-gray rounded-lg border-2 bg-white p-3 pr-1 shadow transition-all hover:shadow-md sm:p-4"
-              role="listitem">
+      <div className="flex gap-10 mt-4 items-center border-gray rounded-lg border-2 bg-white p-3 pr-1 shadow hover:shadow-md sm:p-4" role="listitem">
         
-        <div className="mt-6">
-          <ProgressBar progress={50} label="50% used" />
+        <div className="flex flex-col items-center gap-2">
+          <div style={{ width: 150, height: 150 }}>
+            <CircularProgressbar value={((planCredits - credits) / planCredits) * 100} text={`${((planCredits - credits) / planCredits) * 100}%`} />
+          </div>
+          <p className="text-gray-400"><span className='font-bold'>{planCredits - credits}</span>  out of <span className='font-bold'>{planCredits}</span> used credits</p>
+          <p className="text-gray-400">You have <span className='font-bold'>{credits}</span> credits left.</p>
+          {/* <ProgressBar progress={(100/planCredits)*credits} label={`${credits}/${planCredits} credits`} /> */}
         </div>
-        <div className="grow flex gap-4 sm:items-center flex-col sm:flex-row">
-          <p className="text-gray-400">You have used 1500 credits out of 3000.</p>
-          <p className="text-gray-400">You have 1500 credits left.</p>
+        <div className="grow flex gap-4 sm:items-center flex-col lg:items-start">
+          <p className="text-gray-400">Your are on a <span className='font-bold'>{plan.name}</span> plan which includes:</p>
+          <div className="flex flex-col gap-4 items-left">
+            <p className="text-gray-400"><span className='font-bold'>{planCredits}</span> credits per month</p>
+            <p className="text-gray-400"><span className='font-bold'>{plan.domainsAllowed}</span> domains allowed</p>
+            </div>
+        </div>
+      </div>
+      <div className="flex gap-4 mt-4 items-center border-gray rounded-lg border-2 bg-white p-3 pr-1 shadow hover:shadow-md sm:p-4" role="listitem">
+        <div className="grow flex gap-4 sm:items-center flex-col">
+          <p className="text-gray-400">Upgrade plan to get more credits and domains.</p>
+          <button onClick={handleClick} className={`group flex items-center space-x-3 rounded-md border border-black bg-black px-3 py-2 text-sm font-medium text-white transition-all duration-75 hover:bg-white hover:text-black active:scale-95 `}>Upgrade plan</button>
         </div>
       </div>
     </div>
