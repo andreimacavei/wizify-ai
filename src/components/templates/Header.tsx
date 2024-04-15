1450772757ca33f41c3102d618f3f977d74223eb
andreimacavei <andrei.macavei89@gmail.com>
2024-04-16 02:52:10 +0300

    Remove not used code and minor adjustments

--- src/components/templates/Header.tsx
+++ src/components/templates/Header.tsx
@@ -10,7 +10,7 @@ export default async function Header() {
 	//! TODO: z-index must be removed but first progressbar z-index should be fixed
 	return (
 		<header className="sticky left-0 right-0 top-0 z-20 border-b border-gray-200 bg-white">
-			<div className='flex justify-between items-center mx-auto w-full max-w-screen-xl py-2 px-2.5 lg:px-20'>
+			<div className='flex justify-between items-center mx-auto w-full max-w-screen-xl py-2 px-2.5'>
 				<h1 className="text-3xl">
 					<Link href="/" className='flex flex-row gap-1 items-center font-bold text-blue_ai'>
 					<Image src="/images/logo/logo-1.png" alt='Logo' width={50} height={50} />
